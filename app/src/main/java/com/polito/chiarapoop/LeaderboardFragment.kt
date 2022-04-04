76591ee0461211f52facd9d51615ba3e696cfb94
FRANCESCO FIORELLA s303922 <s303922@studenti.polito.it>
2022-04-04 19:50:20 +0200

    fragment added

---
+++ app/src/main/java/com/polito/chiarapoop/LeaderboardFragment.kt
@@ -0,0 +1,4 @@
+package com.polito.chiarapoop
+
+class LeaderboardFragment {
+}
--- app/src/main/java/com/polito/chiarapoop/MainActivity.kt
+++ app/src/main/java/com/polito/chiarapoop/MainActivity.kt
@@ -2,22 +2,40 @@ package com.polito.chiarapoop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
-    var poop_counter = 0
+
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var achievementFragment: AchievementFragment
+    private lateinit var leaderboardFragment: LeaderboardFragment
+    private lateinit var profileFragment: ProfileFragment
+    private var activeFragment: Fragment? = null
+
+    companion object {
+        private const val ACTIVE_FRAGMENT_KEY = "active_fragment_key"
+        private const val HOME_FRAGMENT_TAG = "home_fragment_tag"
+        private const val ACHIEVEMENT_FRAGMENT_TAG = "achievement_fragment_tag"
+        private const val LEADERBOARD_FRAGMENT_TAG = "leaderboard_fragment_tag"
+        private const val PROFILE_FRAGMENT_TAG = "profile_fragment_tag"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        val poopCounterTV = findViewById<TextView>(R.id.poop_counter)
-        val poopAdder = findViewById<FloatingActionButton>(R.id.poop_adder)
-
-        poopAdder.setOnClickListener {
-            poop_counter++
-            poopCounterTV.text = "Oggi hai fatto $poop_counter poop!"
-        }
+    private fun initFragments() {
+        homeFragment = HomeFragment()
+        achievementFragment = AchievementFragment()
+        leaderboardFragment = LeaderboardFragment()
+        profileFragment = ProfileFragment()
+        activeFragment = homeFragment
+        supportFragmentManager.beginTransaction()
+            .add(R.id.home_fragmentContainerView, profileFragment, PROFILE_FRAGMENT_TAG)
+            .add(R.id.home_fragmentContainerView, leaderboardFragment, LEADERBOARD_FRAGMENT_TAG)
+            .add(R.id.home_fragmentContainerView, achievementFragment, ACHIEVEMENT_FRAGMENT_TAG)
+            .add(R.id.home_fragmentContainerView, homeFragment, HOME_FRAGMENT_TAG)
+            .hide(profileFragment).hide(leaderboardFragment).hide(achievementFragment).commit()
     }
 }
